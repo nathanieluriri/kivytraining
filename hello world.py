130ce53487484a71ac9e6a450c8c6c9c03f5dbc3
Natty_boi <uririnathaniel@gmail.com>
2023-01-07 13:32:57 +0100

    just used box layout for the first time

--- hello world.py
+++ hello world.py
@@ -2,13 +2,19 @@
 # step 2: create the interface
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+
+
 
 class TestApp(App):
     pass
 
-class Interface(Widget):
+
+
+
+class Interface(BoxLayout):
     pass
 
 
