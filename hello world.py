9c5cf6166072274032bd69af9b03504d35d97148
Natty_boi <uririnathaniel@gmail.com>
2023-01-07 21:58:55 +0100

    Float layout is pretty much the same as a main widget

--- hello world.py
+++ hello world.py
@@ -4,6 +4,7 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -16,8 +17,12 @@ class TestApp(App):
 
 
 
-class Interface(AnchorLayout):
-    pass
+class Interface(FloatLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text = "nigga",size_hint =(.34,.30))
+
+        self.add_widget(b1)
 
 
 def color_coverter(x):
