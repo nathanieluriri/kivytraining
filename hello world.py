41ac5299de7aa398a9e11b222686293a4a32a6e1
Natty_boi <uririnathaniel@gmail.com>
2023-01-07 16:10:55 +0100

    used grid layout for the first time and now using the rows to define the layout

--- hello world.py
+++ hello world.py
@@ -3,6 +3,7 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
@@ -14,7 +15,7 @@ class TestApp(App):
 
 
 
-class Interface(BoxLayout):
+class Interface(GridLayout):
     pass
 
 
